6fef4019c2ecfd6f24bb88b8a1876b5cb3ff6b69
Eduardo <eduardo.conti@gazin.com.br>
2022-09-26 22:20:04 -0300

    test: add logger test

---
+++ tests/infra/logger/logger.spec.ts
@@ -0,0 +1,34 @@
+import { Logger } from "@/infra/logger"
+
+interface SutTypes {
+  sut: Logger
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new Logger()
+  return {
+    sut,
+  }
+}
+describe('Logger', () => {
+  it('should execute logger.info', () => {
+    const { sut } = makeSut()
+
+    expect(sut.info('should execute logger.info')
+    ).toBeCalled
+  })
+
+  it('should execute logger.error', () => {
+    const { sut } = makeSut()
+
+    expect(sut.error('should execute logger.error')
+    ).toBeCalled
+  })
+
+  it('should execute logger.system', () => {
+    const { sut } = makeSut()
+
+    expect(sut.system('should execute logger.system')
+    ).toBeCalled
+  })
+})
